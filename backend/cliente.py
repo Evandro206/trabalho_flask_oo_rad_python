a4e95fba5bc9dc5b7e82ffa9b31a8fc334a57bb6
PauloOtavioDuarteRocha <paulotavioduarterocha@hotmail.com>
2025-11-19 17:00:47 -0300

    estrutura

---
+++ backend/cliente.py
@@ -0,0 +1,30 @@
+
+# Definindo a classe Cliente com
+# atributos Nome, CPF, Idade,
+# Profissao e Endereco
+# Objeto não foi instanciado ainda não está em memória
+
+
+class Cliente:
+    Nome: str
+    CPF: str
+    Idade: int
+    Profissao: str
+    Endereco: str
+
+
+
+
+cliente = Cliente()
+cliente.Nome = "Anderson Cruz"
+cliente.CPF = "123.456.789-00"
+cliente.Idade = 30
+cliente.Profissao = "Desenvolvedor"
+cliente.Endereco = "Rua A, 123, Cidade B"
+print(cliente.Nome)
+
+
+
+
+
+
