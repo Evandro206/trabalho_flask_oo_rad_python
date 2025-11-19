c5f01fcb71927bbd0e7c0efb15b02c0b609986e5
PauloOtavioDuarteRocha <paulotavioduarterocha@hotmail.com>
2025-11-19 18:17:19 -0300

    templates

--- main.py
+++ main.py
@@ -6,5 +6,15 @@ app = Flask(__name__)
 def main():
     return render_template(template_name_or_list='home.html')
 
+@app.route('/consulta_saldo')
+def movimentar():
+    return render_template(template_name_or_list='consultaSaldo.html')
+@app.route('/movimentar')
+def consulta_saldo():
+    return render_template(template_name_or_list='movimentacao.html')
+@app.route('/cadastro')
+def cadastro():
+    return render_template(template_name_or_list='cadastro.html')
+
 if __name__ == "__main__":
     app.run(debug=True)
