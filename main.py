822865c06f219aab14b01927d6436de36db85cd1
PauloOtavioDuarteRocha <paulotavioduarterocha@hotmail.com>
2025-11-20 12:50:03 -0300

    comit

--- main.py
+++ main.py
@@ -6,15 +6,31 @@ app = Flask(__name__)
 def main():
     return render_template(template_name_or_list='home.html')
 
+
 @app.route('/consulta_saldo')
-def movimentar():
-    return render_template(template_name_or_list='consultaSaldo.html')
-@app.route('/movimentar')
 def consulta_saldo():
-    return render_template(template_name_or_list='movimentacao.html')
+    return render_template(template_name_or_list='consultaSaldo.html')
+
+@app.route('/movimentar')
+def movimentar():
+    return render_template(template_name_or_list='tipo_de_movimentacao.html')
+
 @app.route('/cadastro')
 def cadastro():
     return render_template(template_name_or_list='cadastro.html')
+
+@app.route('/saldo')
+def saldo():
+    return render_template(template_name_or_list='saldo.html')
+
+@app.route('/depositar')
+def depositar():
+    return render_template(template_name_or_list='depositar.html')
+
+@app.route('/sacar')
+def sacar():
+    return render_template(template_name_or_list='sacar.html')
+
 
 if __name__ == "__main__":
     app.run(debug=True)
